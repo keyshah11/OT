7236176073c61834fb95414076f8260f41e01073
keyshah11 <keyshah11@gmail.com>
2021-03-25 17:32:40 +0530

    False

--- src/test/java/com/ot/testcases/TC_01_OT_HomePage.java
+++ src/test/java/com/ot/testcases/TC_01_OT_HomePage.java
@@ -8,7 +8,7 @@ public class TC_01_OT_HomePage extends OT_BASE {
 
 	OT_Utility ot_utility = new OT_Utility();
 
-	@Test(groups = { "smoke" }, priority = 1, enabled = true)
+	@Test(groups = { "smoke" }, priority = 1, enabled = false)
 	public void TC_Verify_HomePage_Title() throws Exception {
 		ot_utility.Verify_Page_Title(driver.getTitle(), prop.getProperty("home_page_title"), "Home Page");
 	}
